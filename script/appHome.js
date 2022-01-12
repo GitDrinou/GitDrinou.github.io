735770d7689e06e2d75e5739995af7cef1827f6c
GitDrinou <69853179+GitDrinou@users.noreply.github.com>
2022-01-12 16:07:01 +0100

    Version 1.4.0

---
+++ script/appHome.js
@@ -0,0 +1,56 @@
+/**
+ * Variables definition
+ */
+const hamburger = document.querySelector('.navbar-toggler')
+const navbar = document.querySelector('.navbar-collapse')
+const frmContact = document.getElementById('contact-form')
+
+/**
+ * Event click on hamburger button
+ * Show/Not Show nav items
+ */
+hamburger.addEventListener('click', (e) => {
+    e.preventDefault()
+    navbar.classList.toggle('show')
+})
+
+/** Animation  */
+
+const txtAnim = document.querySelector('.text-animation')
+
+let typewriter = new Typewriter(txtAnim, {
+    loop: false,
+    deleteSpeed: 20
+})
+
+typewriter
+    .pauseFor(1200)
+    .changeDelay(20)
+    .typeString('Moi c\'est Sandrine')
+    .pauseFor(300)
+    .typeString('<strong>, Développeuse HTML / CSS</strong> !')
+    .pauseFor(1000)
+    .deleteChars(13)
+    .typeString('<span style="color: #27ae60;"> JavaScript</span> !')
+    .pauseFor(1000)
+    .deleteChars(13)
+    .typeString('<span style="color: orange"> React</span> !')
+    .start()
+
+
+
+/** Contact Form */
+
+frmContact.addEventListener('submit',(e) => {
+    e.preventDefault()
+
+    let contact_title = "Prise de contact"
+    let contact_firstName = document.getElementById('form_firstName').value
+    let contact_lastName = document.getElementById('form_lastName').value
+    let contact_email = document.getElementById('form_email').value
+    let contact_message = document.getElementById('form_message').value
+
+    let contact_body = `Demandeur : %0D%0A Prénom : ${contact_firstName} %0D%0A Nom : ${contact_lastName} %0D%0A E-mail : ${contact_email} %0D%0A %0D%0A Message : ${contact_message} %0D%0A`
+
+    window.open(`mailto:contact.gitDrinou@gmail.com?subject=${contact_title}&body=${contact_body}`);
+})
--- script/appPortfolio.js
+++ script/appPortfolio.js
@@ -1,8 +1,6 @@
-/**
- * Variables definition
- */
-const hamburger = document.querySelector('.navbar-toggler')
-const navbar = document.querySelector('.navbar-collapse')
+/** Definitions */
+
+
 const modalHRNet = document.getElementById("modalHRNet")
 const btnDetailsHRNet = document.getElementById("btnDetailsHRNet")
 const closeDetailsHRNet = document.getElementById("closeDetailsHRNet")
@@ -37,53 +35,21 @@ const modalReservia = document.getElementById("modalReservia")
 const btnDetailsReservia = document.getElementById("btnDetailsReservia")
 const closeDetailsReservia = document.getElementById("closeDetailsReservia")
 
-/**
- * Event click on hamburger button
- * Show/Not Show nav items
- */
-hamburger.addEventListener('click', (e) => {
-    e.preventDefault()
-    navbar.classList.toggle('show')
-})
-
-/** Animation  */
-
-const txtAnim = document.querySelector('.text-animation')
-
-let typewriter = new Typewriter(txtAnim, {
-    loop: false,
-    deleteSpeed: 20
-})
-
-typewriter
-    .pauseFor(1200)
-    .changeDelay(20)
-    .typeString('Moi c\'est Sandrine')
-    .pauseFor(300)
-    .typeString('<strong>, Développeuse HTML / CSS</strong> !')
-    .pauseFor(1000)
-    .deleteChars(13)
-    .typeString('<span style="color: #27ae60;"> JavaScript</span> !')
-    .pauseFor(1000)
-    .deleteChars(13)
-    .typeString('<span style="color: orange"> React</span> !')
-    .start()
-
 
 /** Open / Close Modals */
 
 // Open functions
-btnDetailsHRNet.onclick = () => { modalHRNet.style.display = "block" }
-btnDetailsAgBank.onclick = () => { modalAgBank.style.display = "block" }
-btnDetailsSportSee.onclick = () => { modalSportSee.style.display = "block" }
-btnDetailsKasa.onclick = () => { modalKasa.style.display = "block" }
-btnDetailsLearn.onclick = () => { modalLearn.style.display = "block" }
-btnDetailsBilled.onclick = () => { modalBilled.style.display = "block" }
-btnDetailsLPP.onclick = () => { modalLPP.style.display = "block" }
-btnDetailsFishEye.onclick = () => { modalFishEye.style.display = "block" }
-btnDetailsGameOn.onclick = () => { modalGameOn.style.display = "block" }
-btnDetailsOhMy.onclick = () => { modalOhMy.style.display = "block" }
-btnDetailsReservia.onclick = () => { modalReservia.style.display = "block" }
+btnDetailsHRNet.addEventListener('onclick', () => { modalHRNet.style.display = "block" })
+btnDetailsAgBank.addEventListener('onclick', () => { modalAgBank.style.display = "block" })
+btnDetailsSportSee.addEventListener('onclick', () => { modalSportSee.style.display = "block" })
+btnDetailsKasa.addEventListener('onclick', () => { modalKasa.style.display = "block" })
+btnDetailsLearn.addEventListener('onclick', () => { modalLearn.style.display = "block" })
+btnDetailsBilled.addEventListener('onclick', () => { modalBilled.style.display = "block" })
+btnDetailsLPP.addEventListener('onclick', () => { modalLPP.style.display = "block" })
+btnDetailsFishEye.addEventListener('onclick', () => { modalFishEye.style.display = "block" })
+btnDetailsGameOn.addEventListener('onclick', () => { modalGameOn.style.display = "block" })
+btnDetailsOhMy.addEventListener('onclick', () => { modalOhMy.style.display = "block" })
+btnDetailsReservia.addEventListener('onclick', () => { modalReservia.style.display = "block" })
 
 // Close functions
 closeDetailsHRNet.onclick = () => { modalHRNet.style.display = "none" }
